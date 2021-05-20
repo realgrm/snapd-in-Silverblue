fae2b3ba772e66cdfd941369a59c45f1718e2cd0
Gabriel Mattoso <gabriel.reis.mattoso@gmail.com>
2021-05-20 18:21:42 -0300

    Create snapdSB.sh
    - create symlink /snap
    - bindmount /home
    - replace /var/home to /home in /etc/passwd

---
+++ snapdSB.sh
@@ -0,0 +1,31 @@
+#!/bin/env bash
+
+snapfolder
+homefolder
+passwdhome
+
+# create symlink /snap
+snapfolder () {
+[ -L '/snap' ] && echo "snap symlink already exists" || \
+  ln -s '/var/mnt/snap' '/snap' && echo "symlink /snap created"
+}
+
+# bindmount /home
+homefolder () {
+[ -D '/home' ] && echo "home folder already exists" || \
+[ -L '/home' ] && echo "the symlink /home will be replaced with a bind mount" && bindmounthome ||
+echo "the file named /home will be replaced with a bind mount" && bindmounthome
+}
+
+bindmounthome () {
+  sudo rm -f /home
+  sudo mkdir -p /home
+  sudo mount --bind /var/home /home
+}
+
+# replace /var/home to /home in /etc/passwd
+passwdhome () {
+sudo cp /etc/passwd /etc/passwd.backup
+sudo awk -vold=$"/var/home" -vnew=$"/home" -F: ' BEGIN {OFS = ":"} \
+  {sub(old,new,$6);print}' /etc/passwd > /etc/passwd
+}
