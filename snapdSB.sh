d969821fa1b0828af61993a17a8a2b2e0f917e2c
Gabriel Mattoso <gabriel.reis.mattoso@gmail.com>
2021-05-20 19:07:25 -0300

    Update snapdSB.sh
    fix some typos

--- snapdSB.sh
+++ snapdSB.sh
@@ -1,31 +1,31 @@
 #!/bin/env bash
 
-snapfolder
-homefolder
-passwdhome
-
 # create symlink /snap
-snapfolder () {
+snapfolder(){
 [ -L '/snap' ] && echo "snap symlink already exists" || \
-  ln -s '/var/mnt/snap' '/snap' && echo "symlink /snap created"
+sudo ln -s '/var/mnt/snap' '/snap' && echo "symlink /snap created"
 }
 
 # bindmount /home
-homefolder () {
+homefolder(){
 [ -D '/home' ] && echo "home folder already exists" || \
 [ -L '/home' ] && echo "the symlink /home will be replaced with a bind mount" && bindmounthome ||
 echo "the file named /home will be replaced with a bind mount" && bindmounthome
 }
 
-bindmounthome () {
-  sudo rm -f /home
+bindmounthome(){
+  sudo rm -fr /home
   sudo mkdir -p /home
   sudo mount --bind /var/home /home
 }
 
 # replace /var/home to /home in /etc/passwd
-passwdhome () {
-sudo cp /etc/passwd /etc/passwd.backup
-sudo awk -vold=$"/var/home" -vnew=$"/home" -F: ' BEGIN {OFS = ":"} \
-  {sub(old,new,$6);print}' /etc/passwd > /etc/passwd
+passwdhome(){
+  sudo cp /etc/passwd /etc/passwd.backup
+  sudo awk -vold=$"/var/home" -vnew=$"/home" -F: ' BEGIN {OFS = ":"} \
+    {sub(old,new,$6);print}' /etc/passwd > /etc/passwd
 }
+
+snapfolder
+homefolder
+passwdhome
