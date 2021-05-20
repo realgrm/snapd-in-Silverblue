1c0859102364ea4fe639bcae50070621363726b9
Gabriel Mattoso <gabriel.reis.mattoso@gmail.com>
2021-05-20 20:52:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ SERVICE_FOLDER=/etc/systemd/system/
 
 sudo mkdir -p ${SCRIPT_FOLDER}
 sudo cp -b ${SCRIPT_FILE} ${SCRIPT_FOLDER}
-sudo chmod +x ${SCRIPT_FILE}
+sudo chmod +x ${SCRIPT_FOLDER}${SCRIPT_FILE}
 
 sudo cp -b ${SERVICE_NAME} ${SERVICE_FOLDER}
 sudo ln -bs ${SERVICE_FOLDER}${SERVICE_NAME} ${SERVICE_FOLDER}remote-fs.target.wants/${SERVICE_NAME}
