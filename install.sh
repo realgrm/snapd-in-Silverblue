e55526bc7f7e97fe24f9a60a686162f5d2f6ada4
Gabriel Mattoso <gabriel.reis.mattoso@gmail.com>
2021-05-20 21:11:36 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 #!/bin/env bash
 
-SCRIPT_FOLDER=/opt/bin/
-SCRIPT_FILE=snapdSB.sh
+SCRIPT_FOLDER=/usr/local/bin/
+SCRIPT_FILE=home_snapdSB.sh
 
 SERVICE_FOLDER=/etc/systemd/system/
 SERVICE_NAME=snapdSB.service
 
 
 sudo mkdir -p ${SCRIPT_FOLDER}
-sudo cp -b ${SCRIPT_FILE} ${SCRIPT_FOLDER}
+sudo cp -f ${SCRIPT_FILE} ${SCRIPT_FOLDER}
 sudo chmod +x ${SCRIPT_FOLDER}${SCRIPT_FILE}
 
-sudo cp -b ${SERVICE_NAME} ${SERVICE_FOLDER}
-sudo ln -bs ${SERVICE_FOLDER}${SERVICE_NAME} ${SERVICE_FOLDER}remote-fs.target.wants/${SERVICE_NAME}
+sudo cp -f ${SERVICE_NAME} ${SERVICE_FOLDER}
+sudo ln -sf ${SERVICE_FOLDER}${SERVICE_NAME} ${SERVICE_FOLDER}remote-fs.target.wants/${SERVICE_NAME}
+
+${SCRIPT_FOLDER}${SCRIPT_FILE}
 
 sudo systemctl daemon-reload
-${SCRIPT_FOLDER}${SCRIPT_FILE}
 sudo systemctl start ${SERVICE_NAME}
 
 # rpm-ostree install snapd
