a45db17603641cf04adf30a67631d0341d1d6baf
Gabriel Mattoso <gabriel.reis.mattoso@gmail.com>
2021-06-20 00:42:54 -0300

    correction in install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/env bash
 
 SCRIPT_FOLDER=/opt/snapdSB/
-SCRIPT_FILE=home_snapdSB.sh
+SCRIPT_FILE=snapdSB.sh
 
 SERVICE_FOLDER=/etc/systemd/system/
 SERVICE_NAME=snapdSB.service
 
 
 sudo mkdir -p ${SCRIPT_FOLDER}
-sudo cp -f ${SCRIPT_FILE} ${SCRIPT_FOLDER}
+sudo cp -f * ${SCRIPT_FOLDER}
 sudo chmod +x ${SCRIPT_FOLDER}${SCRIPT_FILE}
 
 sudo cp -f ${SERVICE_NAME} ${SERVICE_FOLDER}
@@ -19,6 +19,8 @@ ${SCRIPT_FOLDER}${SCRIPT_FILE}
 
 sudo systemctl daemon-reload
 sudo systemctl start ${SERVICE_NAME}
+
+sudo mkdir /var/mnt/snap
 
 if ! [[ $(rpm -qa | grep snapd) ]];
 then rpm-ostree install snapd; fi
