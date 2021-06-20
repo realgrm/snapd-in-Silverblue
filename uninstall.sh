23cdbe7ed3ad496e0f93e4ab0ec1fba305f34767
Gabriel Mattoso <gabriel.reis.mattoso@gmail.com>
2021-06-20 16:07:50 -0300

    added unninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+
+SCRIPT_FOLDER=/opt/snapdSB/
+SCRIPT_FILE=snapdSB.sh
+
+SERVICE_FOLDER=/etc/systemd/system/
+SERVICE_NAME=snapdSB.service
+
+if [[ $(rpm -qa | grep snapd) ]]; then rpm-ostree remove snapd; fi
+
+chattr -i /
+	rm -rf /snap
+	if [ -L '/home' ]; then echo "/home is a symlink to /var/home"
+	else
+		umount '/home' && if [ -d '/home' ] && [[ ! $(ls '/home') ]]
+		then
+			sudo rm -rf /home
+			ln -sf /var/home /home
+		else echo "something is wrong"; fi
+	fi
+chattr +i /
+
+sudo rm -rf ${SERVICE_FOLDER}${SERVICE_NAME}
+sudo rm -rf ${SCRIPT_FOLDER}
